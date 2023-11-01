605f12a2d82c931326bf74d02054fc56351e09a4
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-11-01 15:15:17 +0700

    Fix blank bug

--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -8,13 +8,16 @@ import com.example.recycleview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
         val adapterDisaster = DisasterAdapter(generateDummy()) {
-            Toast.makeText(this@MainActivity, "telah diklik", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, "Telah diklik", Toast.LENGTH_SHORT).show()
         }
+
         with(binding) {
             rvDisaster.apply {
                 adapter = adapterDisaster
@@ -23,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun generateDummy(): List<Disaster> {
+    private fun generateDummy(): List<Disaster> {
         return listOf(
             Disaster(nameDisaster = "Tsunami", typeDisaster = "Natural"),
             Disaster(nameDisaster = "Volcanic Eruption", typeDisaster = "Natural"),
@@ -35,5 +38,4 @@ class MainActivity : AppCompatActivity() {
             Disaster(nameDisaster = "War", typeDisaster = "Man-made")
         )
     }
-
 }
